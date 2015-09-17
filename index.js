f9121b1a505d8cdd1392582d8e26cc0718ed0496
Raül Pérez <repejota@gmail.com>
2015-09-17 19:18:48 +0200

    Add /tlks endpoint

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var morgan = require('morgan');
 var fs = require('fs');
 var FileStreamRotator = require('file-stream-rotator');
 
+var libtlks = require('libtlks');
+
 var config = require('./config.json');
 var pkg = require('./package.json');
 
@@ -30,25 +32,41 @@ app.use(morgan('combined', {
 // express.js router class
 var router = express.Router();
 
+/**
+ * Route /teapot
+ */
 router.get('/teapot', function(req, res) {
     res.status(418);
     res.send('I\'m a teapot!');
 });
 
+/**
+ * GET /version
+ */
 router.get('/version', function(req, res) {
     res.send(pkg.version);
 });
 
+/**
+ * GET /talks
+ */
 router.get('/talks', function(req, res) {
-
+    var limit = 25;
+    libtlks.talk.latest(config.mongodb, limit, null, function(err, talks) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        res.json(talks);
+    });
 });
 
 app.use('/', router);
 
 // start the HTTP server
-var server = app.listen(config.port, function() {
+var server = app.listen(config.api.port, function() {
     "use strict";
     var host = server.address().address;
     var port = server.address().port;
-    console.log('tlks.io api listening at http://%s:%s', host, port);
+    console.log('tlks.io api listening at http://%s:%s', host, config.api.port);
 });
